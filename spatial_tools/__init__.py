eaa901025ca4092b347d322760f0d4d2382d48de
jmzhang1911 <jmzhang1911@gmail.com>
2022-11-16 15:51:21 +0800

    add

--- spatial_tools/__init__.py
+++ spatial_tools/__init__.py
@@ -4,4 +4,4 @@ import logging
 
 from spatial_tools.spatial_tools import SpatialApp, SpatialTools
 
-logging.info('Please contact zhangjm@biomarker.com.cn with any questions and suggestions')
+logging.info('Please contact zhangjm@biomarker.com.cn with any questions and suggestions.')
