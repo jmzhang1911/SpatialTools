e6b19b092e0ca59c852276786dcd4ca5509c99dc
jmzhang1911 <jmzhang1911@gmail.com>
2022-12-19 19:41:22 +0800

    add

--- run_spatial_tools_vision.py
+++ run_spatial_tools_vision.py
@@ -3,9 +3,14 @@
 # @Time : 2022/12/19 13:31
 # @Author : jmzhang
 # @Email : zhangjm@biomarker.com.cn
+import logging
 
 import spatial_tools
 
+logging.info('xxxxxx')
 app = spatial_tools.SpatialApp.run_dash(debug=False, return_app=True)
 server = app.server
-app.run_server(debug=False, mode='external')
+
+if __name__ == '__main__':
+
+    app.run_server(debug=False, mode='external')
