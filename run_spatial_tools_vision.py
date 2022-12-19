01a711897511883ce7d74f0b91f75f12ab3faa0b
jmzhang1911 <jmzhang1911@gmail.com>
2022-12-19 13:31:54 +0800

    add

--- run_spatial_tools_vision.py
+++ run_spatial_tools_vision.py
@@ -1,9 +1,8 @@
 #!/share/nas2/genome/biosoft/Python//3.7.3/bin/python3
 # -*- coding: utf-8 -*-
-# @Time : 2022/12/6 15:41
+# @Time : 2022/12/19 13:31
 # @Author : jmzhang
 # @Email : zhangjm@biomarker.com.cn
-
 
 import spatial_tools
 import argparse
@@ -23,5 +22,3 @@ if __name__ == '__main__':
     app = spatial_tools.SpatialApp.run_dash(port=input_args.port, debug=False)
     server = app.server
     app.run_server(debug=False, mode='external')
-
-
